a479062808731704c33dca60da7a1a61a3f0718a
salsabeel noor <salsabeelnoor@gmail.com>
2023-06-07 20:49:45 +0600

    readme updated

--- src/App.js
+++ src/App.js
@@ -11,26 +11,6 @@ function App() {
   );
 }
 
-// function LoadCountries(){
-//   
-//   return (
-//     <div>
-//       <h1>Visiting Every Country of the world!!!</h1>
-//       <h3>Available countries: {countries.length}</h3>
-//       {
-//         countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
-//       }
-//     </div>
-//   )
-// }
 
-// function Country(props){
-//   return (
-//     <div>
-//       <h1>Name: {props.name}</h1>
-//       <h4>Population: {props.population}</h4>
-//     </div>
-//   )
-// }
 
 export default App;
